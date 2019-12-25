197c57586a765e9d1730e81710b3458a6af3f942
yang657850144 <yang657850144@gmail.com>
2019-12-25 23:49:55 +0800

    feat: TextView

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mBtnTextView; // 声明组件
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mBtnTextView = (Button) findViewById(R.id.btn_textview); // 强制类型转换
+        mBtnTextView.setOnClickListener(new View.OnClickListener() { // 注册点击事件
+            @Override
+            public void onClick(View v) {
+                // 跳转到 TextView 演示界面
+                Intent intent = new Intent(MainActivity.this, TextViewActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
