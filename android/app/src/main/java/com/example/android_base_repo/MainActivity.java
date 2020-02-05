729a443fc7fb2e40a685f936e50b2ae393ff557a
robbieyang <robbieyang@tencent.com>
2020-02-05 18:55:06 +0800

    textView

--- android/app/src/main/java/com/example/android_base_repo/MainActivity.java
+++ android/app/src/main/java/com/example/android_base_repo/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    // res/layout/activity_main.xml
     setContentView(R.layout.activity_main);
   }
 }
